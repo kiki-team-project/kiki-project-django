cd7e27647401deabfe0cf5e928afc3fe921db42f
Outeak <yroh05@gmail.com>
2024-02-01 18:59:07 +0900

    feat : 단축키 데이터 프로그램별 저장 및 조회기능 추가

--- kikiproject/shortcuts/management/commands/load_shortcuts.py
+++ kikiproject/shortcuts/management/commands/load_shortcuts.py
@@ -1,13 +1,49 @@
 import os
 import json
 from django.core.management import BaseCommand
-from shortcuts.models import ShortcutKey 
+from shortcuts.models import ShortcutKey, ProgramList
+import base64
+from PIL import Image
+import io
 
 class Command(BaseCommand):
     help = 'Load shortcuts data from a JSON file'
 
     def handle(self, *args, **kwargs):
-        json_file_path = os.path.join(os.path.dirname(__file__), 'shortcuts.json')
+        
+        json_file_path = os.path.join(os.path.dirname(__file__), 'programlist.json')
+
+        try:
+            with open(json_file_path, 'r', encoding='utf-8') as json_file:
+                data = json.load(json_file)
+
+            platforms_data = data.get("platforms", [])
+            
+            for platform in platforms_data:               
+                # 이미지 파일을 열기
+                image_dir = f"shortcuts/management/commands/logo/{platform}.png"
+                
+                with open(image_dir, "rb") as image_file:
+                    # 이미지를 읽고 Base64로 인코딩
+                    encoded_string = base64.b64encode(image_file.read()).decode()
+                
+                ProgramList.objects.create(
+                    platform=platform,
+                    image = encoded_string
+                )
+
+            self.stdout.write(self.style.SUCCESS('Successfully loaded Community data.'))
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f'Failed to load Community data: {str(e)}'))
+        
+        
+        json_file_path = os.path.join(os.path.dirname(__file__), 'figma_shortcut.json')
+        self.save_database(json_file_path, 'figma')
+        
+        json_file_path = os.path.join(os.path.dirname(__file__), 'excel_shortcut.json')
+        self.save_database(json_file_path, 'excel')                         
+            
+    def save_database(self, json_file_path, platform):
 
         try:
             with open(json_file_path, 'r', encoding='utf-8') as json_file:
@@ -24,7 +60,7 @@ class Command(BaseCommand):
                     category=category,
                     keys=keys,
                     description=description,
-                    platform = "figma",
+                    platform = platform,
                     bookmark = 0,
                     index = i
                 )
@@ -32,3 +68,5 @@ class Command(BaseCommand):
             self.stdout.write(self.style.SUCCESS('Successfully loaded shortcuts data.'))
         except Exception as e:
             self.stdout.write(self.style.ERROR(f'Failed to load shortcuts data: {str(e)}'))
+        
+        
