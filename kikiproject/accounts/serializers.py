4b0163a70dcda4502d67e6727a185ea14ef2388e
ltekean <ltekean@gmail.com>
2024-03-30 18:15:14 +0900

    chore: 불필요 함수 삭제

--- kikiproject/accounts/serializers.py
+++ kikiproject/accounts/serializers.py
@@ -1,19 +1,11 @@
-from django.core.mail import send_mail
 from django.core.validators import validate_email
 from django.core.exceptions import ValidationError
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.conf import settings
-from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.serializers import (
     ModelSerializer,
-    SerializerMethodField,
 )
 from .models import CustomUser
-from .emails import account_activation_token
 
 class UserSerializer(ModelSerializer):
     # is_host = SerializerMethodField()
@@ -36,25 +28,6 @@ class UserSerializer(ModelSerializer):
         user = CustomUser(**validated_data)
         user.set_password(password)
         user.save()
-        print(password)
-
-        # html = render_to_string(
-        #     "accounts/email_register.html",
-        #     {
-        #         "backend_base_url": settings.BACKEND_BASE_URL,
-        #         "uidb64": urlsafe_base64_encode(force_bytes(user.id)).encode().decode(),
-        #         "token": account_activation_token.make_token(user),
-        #         "user": user,
-        #     },
-        # )
-        # to_email = user.email
-        # send_mail(
-        #     "안녕하세요 키키입니다. 인증메일이 도착했어요!",
-        #     "_",
-        #     settings.DEFAULT_FROM_MAIL,
-        #     [to_email],
-        #     html_message=html,
-        # )
         return user
     
     def validate(self, attrs): # 중복 체크
@@ -71,19 +44,6 @@ class UserSerializer(ModelSerializer):
             raise serializers.ValidationError("Enter a valid email address.")
 
         return value
-
-
-class PublicUserSerializer(ModelSerializer):
-    # is_host = SerializerMethodField()
-    # total_bookmark_articles = SerializerMethodField()
-
-    class Meta:
-        model = CustomUser
-        exclude = (
-            "groups",
-            "user_permissions",
-            "password",
-        )
     
 
 class UserDetailSerializer(ModelSerializer):
--- kikiproject/accounts/views.py
+++ kikiproject/accounts/views.py
@@ -2,33 +2,24 @@ import re
 import requests
 
 from django.conf import settings
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
-from django.utils.encoding import force_bytes, force_str
-from django.shortcuts import redirect
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
 from rest_framework import status, generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import (
     PermissionDenied,
-    ParseError,
 )
 from rest_framework.generics import get_object_or_404
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from accounts.serializers import (
     UserSerializer,
-    PublicUserSerializer,
     UserDetailSerializer,
     CustomTokenObtainPairSerializer,
     UserProfilePhotoUpdateSerializer
 )
 from accounts import serializers
 from accounts.models import CustomUser
-from accounts.validators import validate_password
-from accounts.emails import account_activation_token
 
 # Create your views here.
 class UserView(APIView):
@@ -101,59 +92,11 @@ class UserView(APIView):
             )
         
 
-
-class UserSignUpPermitView(APIView):
-    def get(self, request, uidb64, token):
-        try:
-            uid = force_str(urlsafe_base64_decode(uidb64))
-            user = CustomUser.objects.get(pk=uid)
-            if account_activation_token.check_token(user, token):
-                CustomUser.objects.filter(pk=uid).update(is_active=True)
-
-                html = render_to_string(
-                    "users/email_welcome.html",
-                    {
-                        "front_base_url": settings.FRONT_BASE_URL,
-                        "user": user,
-                    },
-                )
-                to_email = user.email
-                send_mail(
-                    "안녕하세요 키키입니다. 회원가입을 축하드립니다!",
-                    "_",
-                    settings.DEFAULT_FROM_MAIL,
-                    [to_email],
-                    html_message=html,
-                )
-                return redirect(f"{settings.FRONT_BASE_URL}/login.html")
-            return Response({"error": "AUTH_FAIL"}, status=status.HTTP_400_BAD_REQUEST)
-        except:
-            return Response({"error": "KEY_ERROR"}, status=status.HTTP_400_BAD_REQUEST)
-        
-
 class LoginView(TokenObtainPairView):
     '''
     TokenObtainPairView를 커스터마이징함
     '''
     serializer_class = CustomTokenObtainPairSerializer
-
-
-class UserResetPasswordPermitView(APIView):
-    '''
-    비밀번호 초기화 클래스
-    토큰이 유효할 경우, 비밀번호 변경 페이지로 리다이렉트
-    '''
-    def get(self, request, uidb64, token):
-        try:
-            uid = force_str(urlsafe_base64_decode(uidb64))
-            user = CustomUser.objects.get(pk=uid)
-            if account_activation_token.check_token(user, token):
-                return redirect(
-                    f"{settings.FRONT_BASE_URL}/users/password_change.html?uid={uid}&uidb64={uidb64}&token={token}"
-                )
-            return Response({"error": "AUTH_FAIL"}, status=status.HTTP_400_BAD_REQUEST)
-        except KeyError:
-            return Response({"error": "KEY_ERROR"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class KakaoLoginView(APIView):
@@ -277,146 +220,6 @@ def social_login_validate(**kwargs):
         )
 
 
-class ResetPasswordView(APIView):
-    """
-    비밀번호 찾기.
-    이메일 인증하면 비밀번호 재설정할 기회를 준다.
-    Post : 비밀번호 초기화 템플릿 전송 후 초기화
-    Put : 새 비밀번호로 수정
-    """
-
-
-    def post(self, request):
-        try:
-            user_email = request.data.get("email")
-            user = CustomUser.objects.get(username=user_email)
-            if user:
-                if user.login_type == "normal":
-                    html = render_to_string(
-                        "accounts/email_password_reset.html",
-                        {
-                            "backend_base_url": settings.BACKEND_BASE_URL,
-                            "uidb64": urlsafe_base64_encode(force_bytes(user.id))
-                            .encode()
-                            .decode(),
-                            "token": account_activation_token.make_token(user),
-                            "user": user,
-                        },
-                    )
-                    to_email = user.email
-                    send_mail(
-                        "안녕하세요 키키입니다. 비밀번호 초기화 메일이 도착했어요!",
-                        "_",
-                        settings.DEFAULT_FROM_MAIL,
-                        [to_email],
-                        html_message=html,
-                    )
-                    return Response(
-                        {"message": "이메일 전송 완료!"}, status=status.HTTP_200_OK
-                    )
-                else:
-                    return Response(
-                        {"error": "해당 이메일은 소셜로그인 이메일입니다!"},
-                        status=status.HTTP_400_BAD_REQUEST,
-                    )
-        except CustomUser.DoesNotExist:
-            return Response(
-                {"error": "해당 이메일에 일치하는 사용자가 없습니다!"}, status=status.HTTP_400_BAD_REQUEST
-            )
-
-    def put(self, request):
-        uidb64 = request.data.get("uidb64")
-        token = request.data.get("token")
-        email = request.data.get("email")
-        new_first_password = request.data.get("new_first_password")
-        new_second_password = request.data.get("new_second_password")
-        try:
-            user = CustomUser.objects.get(email=email)
-        except CustomUser.DoesNotExist:
-            return Response(
-                {"error": "일치하는 유저가 존재하지 않습니다!"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        if not uidb64 or not token:
-            return Response({"error": "잘못된 접근입니다!"}, status=status.HTTP_403_FORBIDDEN)
-        if not new_first_password or not new_second_password:
-            return Response(
-                {"error": "비밀번호 입력은 필수입니다!"}, status=status.HTTP_400_BAD_REQUEST
-            )
-        if new_first_password != new_second_password:
-            return Response(
-                {"error": "비밀번호가 일치하지 않습니다!"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        if len(new_second_password) < 6:
-            return Response(
-                {"error": "비밀번호는 6자리 이상이어야 합니다."}, status=status.HTTP_400_BAD_REQUEST
-            )
-        if account_activation_token.check_token(user, token):
-            try:
-                user.set_password(new_second_password)
-                user.is_active = True
-                user.save()
-                return Response(
-                    {"message": "비밀번호가 재설정 되었습니다!"}, status=status.HTTP_200_OK
-                )
-            except Exception:
-                raise ParseError
-        else:
-            return Response({"error": "잘못된 접근입니다!"}, status=status.HTTP_403_FORBIDDEN)
-
-
-class ChangePasswordView(APIView):
-    """
-    노말 로그인 회원만 비번 바꾸기
-    old_password : 구 비밀번호
-    new_password : 새 비밀번호
-    new_password2 : 새 비밀번호 확인용
-    """
-
-    permission_classes = [IsAuthenticated]
-
-    def put(self, request):
-        user = request.user
-        if user.login_type == "normal":
-            old_password = request.data.get("old_password")
-            new_password = request.data.get("new_password")
-            new_password2 = request.data.get("new_password2")
-            if not old_password or not new_password or not new_password2:
-                return Response(
-                    {"error": "비밀번호입력은 필수입니다!"}, status=status.HTTP_400_BAD_REQUEST
-                )
-            if old_password == new_password:
-                return Response(
-                    {"error": "예전 비밀번호와 새 비밀번호가 일치합니다!"},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            if new_password != new_password2:
-                return Response(
-                    {"error": "비밀번호가 일치하지 않습니다!"}, status=status.HTTP_400_BAD_REQUEST
-                )
-            if len(new_password) < 6:
-                return Response(
-                    {"error": "비밀번호는 6자리 이상이어야 합니다."},
-                    status=status.HTTP_400_BAD_REQUEST,
-                )
-            if user.check_password(old_password):
-                user.set_password(new_password)
-                user.save()
-                return Response(
-                    {"message": "비밀번호가 변경되었습니다!"}, status=status.HTTP_200_OK
-                )
-            else:
-                return Response(
-                    {"error": "현재 비밀번호가 일치하지 않습니다!"}, status=status.HTTP_400_BAD_REQUEST
-                )
-        else:
-            return Response(
-                {"error": "비밀번호 변경은 일반 로그인 계정만 가능합니다!"},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
-
 class UserProfilePhotoUpdateAPIView(generics.UpdateAPIView):
     """
     유저 프로필 사진만 수정
@@ -431,29 +234,11 @@ class UserProfilePhotoUpdateAPIView(generics.UpdateAPIView):
 
 class UserDetailView(APIView):
     '''
-    마이페이지 관련 클래스
-    get : 유저 프로필 조회
+    사용자 관련 클래스
     put : 유저 프로필(닉네임, 사진) 수정
     patch : 유저 삭제
     '''
     permission_classes = [IsAuthenticated]
-
-    def get(self, request, user_id):
-        # """유저 프로필 조회 주석 추가 예정"""
-
-        user = get_object_or_404(CustomUser, id=user_id)
-        if request.user.id == user_id:
-            serializer = UserSerializer(
-                user,
-                context={"request": request},
-            )
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            serializer = PublicUserSerializer(
-                user,
-                context={"request": request},
-            )
-            return Response(serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, user_id):
         user = get_object_or_404(CustomUser, id=user_id)
